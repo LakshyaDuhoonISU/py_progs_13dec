4ea0cc7b10efbfa8c8ce848ac383cd0347d5c5ce
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-01-11 22:01:12 +0530

    Update and rename program15.py to 15UpperLowercaseCount.py

--- 15UpperLowercaseCount.py
+++ 15UpperLowercaseCount.py
@@ -1,4 +1,4 @@
-#wap function function that accepts a string and counts the number of upper and lower case letters
+#wap function that accepts a string and counts the number of upper and lower case letters
 def lucount(a):
     lcount=0
     ucount=0
